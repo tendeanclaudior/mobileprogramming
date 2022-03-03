4f5dd1713ed7e0bbdc842739d7150ca00d18c3d3
tendeanclaudior <tendeanclaudio1234@gmail.com>
2022-03-03 13:59:55 +0800

    commit text 1

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Text 1</Text>
+      </View>
+    );
+  }
+}
