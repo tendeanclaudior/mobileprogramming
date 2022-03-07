b0b79cff7762b9fab222da5040964d270d6d6852
hiskiabororing <tommybororing@gmail.com>
2022-03-07 14:45:12 +0800

    menambahkan index

--- exercise06/Components/index.js
+++ exercise06/Components/index.js
@@ -2,3 +2,17 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import State from './State';
 import Props from './Props';
+
+export default class App extends Component {
+    render() {
+        return (
+            <State /> // State adalah data private sebuah component.
+
+            <Props saya='Claudio Tendean'/>
+            <Props saya='Hiskia Bororing'/>
+            <Props saya='Tesalonika Wangania'/>
+            <Props saya='Anatasya Kuron'/>
+            <Props saya='Vini Tandiseru'/>
+        ) // Props adalah data yang memiliki fungsi ganda yaitu selain untuk menyimpan data, Props juga berfungsi untuk “melempar data” antar components view jika ada perubahan data di state.
+    }
+}
